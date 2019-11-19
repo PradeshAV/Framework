43e95aadcc0158fc86b90302064078b03378b1f3
Pradesh <pradeeavg@gmail.com>
2019-11-19 18:20:00 +0530

    Few codings are ultered

--- Helloworld/src/com/helloworld/Helloworld.java
+++ Helloworld/src/com/helloworld/Helloworld.java
@@ -5,7 +5,7 @@ public class Helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("Welcome pradesh");
-	System.out.println("Welcome silambu");
+
 	
 
 	}
